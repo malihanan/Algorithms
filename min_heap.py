b52d1c023fc16ec1123404a4f7a8b928e7252a64
Maliha B Nan <malihanan09@gmail.com>
2020-06-20 19:50:34 +0530

    Update min_heap.py

--- min_heap.py
+++ min_heap.py
@@ -31,9 +31,23 @@ class MinHeap:
 	def get_min(self):
 		return self.ans[0]
 
+	def insert(self, k):
+		self.size+=1
+		self.arr.append(k)
+
+		ik = self.size - 1
+		ip = (ik - 1)//2
+		while ik!=0 and self.arr[ip] > self.arr[ik]:
+			self.arr[ip], self.arr[ik] = self.arr[ik], self.arr[ip]
+			ik = ip
+			ip = (ik-1)//2
+
 if __name__ == '__main__':
 	a = [7,3,6,2,5,9,4,6,1]
 	print(a)
 	m = MinHeap(a)
+	m.insert(4)
+	m.insert(0)
+	print(a)
 	for i in range(len(a)):
-		print(m.extract_min())
+		print(m.extract_min(), end=" ")
